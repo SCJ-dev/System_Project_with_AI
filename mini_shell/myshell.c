ae3f49d13f70c45738ab16096aae0f6f942953ec
SCJ-dev <sc262352@gmail.com>
2025-12-08 20:25:08 +0900

    step 1 : git push

---
+++ mini_shell/myshell.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+	char in[1024]; // user 
+
+	for (;;){
+		printf("MY_SHELL$ ");
+		if (fgets(in, 1024, stdin) == NULL){
+		  break;
+		}
+		in[strcspn(in, "\n")] = 0;
+		
+		if(strcmp(in, "exit") == 0){
+		printf("program off\n");
+		break;
+		}
+		printf("your input command : %s\n", in);
+      
+        }
+}
